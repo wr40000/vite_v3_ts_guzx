c6753508bed8b12a8bb4e9efbb59dc4dd8d0fbe5
wr <2119495210@qq.com>
2023-05-18 14:31:34 +0800

    准备husky

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dsad">
+    <div class="box1">dasdadasda</div>
+    dasdas
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dsad {
+  width: 200px;
+  height: 200px;
+  background-color: #3d0c0c;
+  .box1 {
+    width: 100px;
+    height: 100px;
+    background-color: #53e9c3;
+  }
+}
+</style>
